28ffd9640dfeb0b6c293367134764c7c84e17201
CaptMavvvvv <deathmoonza2017@gmail.com>
2024-07-13 20:03:51 +0700

    save for test

--- lecture02/class.py
+++ lecture02/class.py
@@ -2,6 +2,6 @@ boy = int(input("Enter boy college numbers: "))
 girl = int(input("Enter woman college numbers: "))
 total = boy + girl
 
-boyper = (boy*100)/total
+boyper = (boy/total)*100
 
 print("Percentage of this class is: ", boyper)
