9c75fd657d29045255c6662b22a13c1f91b22b13
CaptMavvvvv <deathmoonza2017@gmail.com>
2024-08-08 11:47:05 +0700

    save

--- HW-01.py
+++ HW-01.py
@@ -1,17 +1,16 @@
 def format_strings(args):
-    if len(args) == 2: # Check if the number of arguments is exactly two
-        result = '-'.join(args) # Join the two arguments with hyphen
-    else: # If the number of arguments is not two
-        result = ''.join(args) # Join all arguments without any seperators
-    result = result.upper() # Turn the resulting string to uppercase
-    return result # Return the final uppercase string
+    if len(args) == 1:
+        return args[0].replace(' ', '-').upper()
 
-if __name__ == '__main__': # Ensures the following code only runs when the script is executed directly
-    result = format_strings(["Hello", "world", "this", "is", "a", "test"])
-    print(result) # Output: "HELLOWORLDTHISISATEST"
+    result = ''.join(args).upper()
+    return result
 
-    result = format_strings(["Python", "is", "fun"])
-    print(result) # Output: "PYTHONISFUN"
+if __name__ == '__main__':
+  result = format_strings("Hello", "world", "this", "is", "a", "test")
+  print(result)  # Output: HELLOWORLDTHISISATEST
 
-    result = format_strings(["Hello", "world"])
-    print(result) # Output: "HELLO- WORLD"
+  result = format_strings("Python", "is", "fun")
+  print(result)  # Output: PYTHONISFUN
+
+  result = format_strings("Hello World")
+  print(result)  # Output: HELLO-WORLD
