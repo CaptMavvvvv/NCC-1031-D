380a36ad911865db9ce08b0115d64f8416f0bea1
CaptMavvvvv <deathmoonza2017@gmail.com>
2024-08-22 11:02:56 +0700

    save for practice

--- ComproPractice2.py
+++ ComproPractice2.py
@@ -1,5 +1,7 @@
 def find_repeated_substrings(s: str) -> list:
-    pass
+    count_strs = {}
+    for string in strings:
+        if str
 
 print(find_repeated_substrings("banana"))
 print(find_repeated_substrings("abcdefg"))
