5879d95ca3f2e4331ee24f4dd224eba8270a7916
CaptMavvvvv <deathmoonza2017@gmail.com>
2024-09-05 11:23:49 +0700

    save for lecture 8

--- lecture08/filewrite.py
+++ lecture08/filewrite.py
@@ -1,9 +1,10 @@
 def main():
-    outfile = open('philosophers.txt', 'w')
+    outfile = open('philosophers.txt', 'a')
 
     outfile.write('John Locke\n')
     outfile.write('David Hume\n')
     outfile.write('Edmund Burke\n')
+    outfile.write('Supacha\n')
 
     outfile.close()
 
