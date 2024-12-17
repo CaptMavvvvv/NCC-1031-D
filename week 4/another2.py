8ad7583a962055e8fa1da5df28efac941ec7cd1d
CaptMavvvvv <deathmoonza2017@gmail.com>
2024-12-17 16:51:23 +0700

    saveeeeeeeeeeeeee

--- week 4/another2.py
+++ week 4/another2.py
@@ -1,8 +1,25 @@
-start_num = int(input("S num: "))
-end_num = int(input("E num: "))
+start_num = int(input("Start num: "))
+end_num = int(input("End num: "))
 
-count = 0
+# Input for divisible number (condition)
+divisible_by = int(input("Enter a number to divide by: "))
 
-for i in range(start_num+1, end_num):
-    if count % 3 == 0:
-        print(count)
+total_loops = 5  # Number of loops before asking the user
+
+while True:
+    count = 0
+    for num in range(start_num + 1, end_num):
+        if num % divisible_by == 0:
+            print(f"Number: {num}")
+            count += 1
+        
+        if count == total_loops:
+            break
+
+    # Ask user if they want to continue after 5 loops
+    choice = int(input("Wanna continue or exit? (Exit=0, Continue=1): "))
+    if choice == 0:
+        print("Exiting the loop.")
+        break
+    elif choice == 1:
+        continue
