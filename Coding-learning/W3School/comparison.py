4f5ce90fae92028128b5b5881e75d2042c9920c8
CaptMavvvvv <deathmoonza2017@gmail.com>
2026-07-14 00:12:00 +0700

    Save for W3School 13/07/26

---
+++ Coding-learning/W3School/comparison.py
@@ -0,0 +1,4 @@
+x = 5
+
+print(1 < x < 10)
+print(1 < x and x < 10)
---
+++ Coding-learning/W3School/list.py
@@ -0,0 +1,4 @@
+mylist = ["apple", "banana", "cherry"]
+thislist = list(("apple", "banana", "cherry"))
+print(type(mylist))
+print(thislist)
