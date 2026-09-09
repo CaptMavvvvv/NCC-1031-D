d8b973c46438e511cecfb73c7573eb328574016f
CaptMavvvvv <deathmoonza2017@gmail.com>
2026-09-09 09:37:10 +0700

    save for lecture 7

---
+++ private/Compro-3/lecture7-set-dict/subSet-superSet.py
@@ -0,0 +1,13 @@
+setA = {1,2,3,4}
+setB = {2,3}
+setC = {1,2,3,4}
+setD = {1,2,3,4,5}
+
+print(setA >= setB)
+print(setB <= setA)
+
+print(setA > setB)
+print(setB < setA)
+
+print(setA == setC)
+print(setB <= setD and setB != setD)
